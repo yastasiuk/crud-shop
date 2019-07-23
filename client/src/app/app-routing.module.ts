671610832546dd1501a3aee37779f112ec431ede
Yaroslav Stasiuk <yastasyuk@gmail.com>
2019-07-23 23:41:10 +0300

    Initial client commit

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/list/product-list.component';
+
+const routes: Routes = [
+    { path: '**', component: ProductListComponent},
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes),
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
