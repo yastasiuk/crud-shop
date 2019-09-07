e4ead6e8b6ff3aa69bfd4d5d5f6087c0e24993d3
Yaroslav Stasiuk <yastasyuk@gmail.com>
2019-09-07 03:00:07 +0300

    [Server] Added support for servlets

--- server/src/main/java/com/yastasiuk/shop/Application.java
+++ server/src/main/java/com/yastasiuk/shop/Application.java
@@ -2,19 +2,26 @@ package com.yastasiuk.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.yastasiuk.shop.storage.StorageService;
 import com.yastasiuk.shop.storage.StorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 
     @Bean
